dc4a649d6882020f8fcb97af407745be8366cb5b
Bipul Devashish <bipuldevashish@gmail.com>
2018-05-14 09:58:37 +0530

    intent added..

--- app/src/main/java/com/bipuldevashish/justjava/MainActivity.java
+++ app/src/main/java/com/bipuldevashish/justjava/MainActivity.java
@@ -1,17 +1,21 @@
 package com.bipuldevashish.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    String hasWhippedCream,name;
+
+    String hasWhippedCream,hasChocolate;
     int quantity = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +39,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view){
-        int price = calculatePrice();
+        String name;
         EditText editText = findViewById(R.id.name_edittext);
         name = editText.getText().toString();
         CheckBox checkBox = findViewById(R.id.notify_me_checkbox);
+        CheckBox checkBox_chocolate = findViewById(R.id.notify_me_checkbox_chocolate);
         if (checkBox.isChecked()){
             hasWhippedCream = "yes";
         }
         else{
             hasWhippedCream = "No";
         }
-        orderSummary(price,quantity,name,hasWhippedCream);
+
+        if (checkBox_chocolate.isChecked()){
+            hasChocolate = "yes";
+        }
+        else{
+            hasChocolate = "No";
+        }
+
+
+        int price = calculatePrice(hasChocolate,hasWhippedCream);
+        orderSummary(price,quantity,name,hasWhippedCream,hasChocolate);
     }
 
-    public void orderSummary(int value,int quantity,String name,String cream){
+    public void orderSummary(int value,int quantity,String name,String cream,String choco){
+             String Data = "Name - "+ name + "\n" + "Quantity - " + quantity + "\n" +
+            "Wipped cream - " + cream + "\n" + "Chocolate -" + choco +
+                "\n" + "Total - $" + value + "\n" +"Thank you!";
         TextView textView = findViewById(R.id.orderSummary_text_view);
-        textView.setText("Name - "+ name + "\n" +"Quantity - " + quantity + "\n"
-                +"Wipped cream - " + cream +  "\n" + "Total - $" + value + "\n" +"Thank you!");
+        textView.setText(Data);
+        String subject = "CoFFee order for " + name;
+        String address[] = new String[]{"aryanbipul@gmail.com"};
+        composeEmail(address,subject,Data);
     }
 
-    private int calculatePrice() {
-        int price = quantity *5;
+    public void composeEmail(String[] addresses, String subject, String data) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" )); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, data);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private int calculatePrice(String whipped, String chocolate) {
+        int price = 0;
+        if (hasWhippedCream == "yes"){
+            price = price + (quantity*1);
+        }
+        if (hasChocolate == "yes"){
+            price = price + (quantity*2);
+        }
+        price = price +(quantity *5);
         return price;
     }
 
